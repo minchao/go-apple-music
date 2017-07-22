ab8e28ce592e172c5389c7a749b2a1c9c8fa60e7
minchao <minchao.220@gmail.com>
2017-07-22 19:43:07 +0800

    Add search for CatalogService

---
+++ catalog_search.go
@@ -0,0 +1,107 @@
+package applemusic
+
+import (
+	"context"
+	"fmt"
+)
+
+// SearchResults represents a results, that contains a map of search results.
+// The members of the results object are the types of resources and the value for each is a Response Root object.
+type SearchResults struct {
+	Activities    *Activities    `json:"activities,omitempty"`
+	Albums        *Albums        `json:"albums,omitempty"`
+	AppleCurators *AppleCurators `json:"apple-curators,omitempty"`
+	Artists       *Artists       `json:"artists,omitempty"`
+	Curators      *Curators      `json:"curators,omitempty"`
+	MusicVideos   *MusicVideos   `json:"music-videos,omitempty"`
+	Playlists     *Playlists     `json:"playlists,omitempty"`
+	Stations      *Stations      `json:"stations,omitempty"`
+	Songs         *Songs         `json:"songs,omitempty"`
+}
+
+// Search represents the result of search for resources.
+type Search struct {
+	Results SearchResults `json:"results"`
+}
+
+// SearchOptions specifies the parameters to search the catalog.
+type SearchOptions struct {
+	// The entered text for the search with ‘+’ characters between each word,
+	// to replace spaces (for example term=james+br).
+	Term string `url:"term"`
+
+	// (Optional) The localization to use, specified by a language tag.
+	// The possible values are in the supportedLanguageTags array belonging to the Storefront object specified by storefront.
+	// Otherwise, the storefront’s defaultLanguageTag is used.
+	Language string `url:"l,omitempty"`
+
+	// (Optional) The limit on the number of objects, or number of objects in the specified relationship, that are returned.
+	// The default value is 5 and the maximum value is 25.
+	Limit int `url:"limit,omitempty"`
+
+	// (Optional; valid only with types specified) The next page or group of objects to fetch.
+	Offset int `url:"offset,omitempty"`
+
+	// (Optional) The list of the types of resources to include in the results.
+	Types string `url:"types,omitempty"`
+}
+
+// Search searches the catalog using a query.
+func (s *CatalogService) Search(ctx context.Context, storefront string, opt *SearchOptions) (*Search, *Response, error) {
+	u := fmt.Sprintf("v1/catalog/%s/search", storefront)
+	u, err := addOptions(u, opt)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	search := &Search{}
+	resp, err := s.client.Do(ctx, req, search)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return search, resp, nil
+}
+
+type SearchHintsOptions struct {
+	Term     string `url:"term"`
+	Language string `url:"l,omitempty"`
+	Limit    int    `url:"limit,omitempty"` // (Optional) The number of search terms to be returned. The default value is 10.
+	Types    string `url:"types,omitempty"`
+}
+
+type SearchHintsResults struct {
+	Terms []string `json:"terms"`
+}
+
+// SearchHints represents the result of search hints.
+type SearchHints struct {
+	Results SearchHintsResults `json:"results"`
+}
+
+// SearchHints fetches the search term results for a hint.
+func (s *CatalogService) SearchHints(ctx context.Context, storefront string, opt *SearchHintsOptions) (*SearchHints, *Response, error) {
+	u := fmt.Sprintf("v1/catalog/%s/search/hints", storefront)
+	u, err := addOptions(u, opt)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	searchHints := &SearchHints{}
+	resp, err := s.client.Do(ctx, req, searchHints)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return searchHints, resp, nil
+}
---
+++ catalog_search_test.go
@@ -0,0 +1,236 @@
+package applemusic
+
+import (
+	"context"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestCatalogService_Search(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v1/catalog/us/search", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		testFormValues(t, r, values{
+			"term":  "james+brown",
+			"limit": "1",
+			"types": "artists,albums",
+		})
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(searchJSON)
+	})
+
+	opt := &SearchOptions{
+		Term:  "james+brown",
+		Limit: 1,
+		Types: "artists,albums",
+	}
+
+	got, _, err := client.Catalog.Search(context.Background(), "us", opt)
+	if err != nil {
+		t.Errorf("Catalog.Search returned error: %v", err)
+	}
+	if want := search; !reflect.DeepEqual(got, want) {
+		t.Errorf("Catalog.Search = %+v, want %+v", got, want)
+	}
+}
+
+func TestCatalogService_SearchHints(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v1/catalog/us/search/hints", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		testFormValues(t, r, values{
+			"term":  "james+brown",
+			"limit": "1",
+			"types": "artists,albums",
+		})
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{
+    "results": {
+        "terms": [
+            "love kendrick lamar",
+            "love on the brain",
+            "love galore",
+            "love",
+            "love songs",
+            "love the way you lie",
+            "loverboy",
+            "lovely day",
+            "love me like you do",
+            "love me now"
+        ]
+    }
+}`))
+	})
+
+	opt := &SearchHintsOptions{
+		Term:  "james+brown",
+		Limit: 1,
+		Types: "artists,albums",
+	}
+
+	searchHints := &SearchHints{
+		Results: SearchHintsResults{
+			Terms: []string{
+				"love kendrick lamar",
+				"love on the brain",
+				"love galore",
+				"love",
+				"love songs",
+				"love the way you lie",
+				"loverboy",
+				"lovely day",
+				"love me like you do",
+				"love me now",
+			},
+		},
+	}
+
+	got, _, err := client.Catalog.SearchHints(context.Background(), "us", opt)
+	if err != nil {
+		t.Errorf("Catalog.SearchHints returned error: %v", err)
+	}
+	if want := searchHints; !reflect.DeepEqual(got, want) {
+		t.Errorf("Catalog.SearchHints = %+v, want %+v", got, want)
+	}
+}
+
+var searchJSON = []byte(`{
+    "results": {
+        "albums": {
+            "href": "/v1/catalog/us/search?types=albums&term=james+brown&limit=1",
+            "next": "/v1/catalog/us/search?types=albums&term=james+brown&offset=1",
+            "data": [
+                {
+                    "id": "900721190",
+                    "type": "albums",
+                    "href": "/v1/catalog/us/albums/900721190",
+                    "attributes": {
+                        "artwork": {
+                            "width": 1400,
+                            "height": 1400,
+                            "url": "https://is1-ssl.mzstatic.com/image/thumb/Music4/v4/76/85/e5/7685e5c8-9346-88db-95ff-af87bf84151b/source/{w}x{h}bb.jpg",
+                            "bgColor": "ffffff",
+                            "textColor1": "0a0a09",
+                            "textColor2": "2a240f",
+                            "textColor3": "3b3b3a",
+                            "textColor4": "544f3f"
+                        },
+                        "artistName": "James Brown",
+                        "isSingle": false,
+                        "url": "https://itunes.apple.com/us/album/20-all-time-greatest-hits/id900721190",
+                        "isComplete": true,
+                        "genreNames": [
+                            "Soul",
+                            "Music",
+                            "R&B/Soul",
+                            "Funk"
+                        ],
+                        "trackCount": 20,
+                        "releaseDate": "1991-01-01",
+                        "name": "20 All-Time Greatest Hits!",
+                        "copyright": "℗ 1991 Universal Records, a Division of UMG Recordings, Inc.",
+                        "playParams": {
+                            "id": "900721190",
+                            "kind": "album"
+                        }
+                    }
+                }
+            ]
+        },
+        "artists": {
+            "href": "/v1/catalog/us/search?types=artists&term=james+brown&limit=1",
+            "next": "/v1/catalog/us/search?types=artists&term=james+brown&offset=1",
+            "data": [
+                {
+                    "id": "117118",
+                    "type": "artists",
+                    "href": "/v1/catalog/us/artists/117118",
+                    "attributes": {
+                        "url": "https://itunes.apple.com/us/artist/james-brown/id117118",
+                        "name": "James Brown",
+                        "genreNames": [
+                            "R&B/Soul"
+                        ],
+                        "editorialNotes": {
+                            "standard": "The Godfather of Soul, The Hardest Working Man in Show Business, Soul Brother Number One . . . Mountain-of-a-man nicknames, no doubt, but not one of them ever did James Brown the justice he deserved. JB’s influence is so large it’s plain impossible to imagine what music today would sound like without him. James didn’t just master soul and funk; he invented them. From the on-bended-knee plea called “Try Me” straight to ground zero of the hip-hop revolution, where his screams, grunts, and funky drummer backboned every cut worth mentioning, Brown was soul power itself, an inspiration to an entire nation. Now that Mr. Dynamite has taken his last sweat-drenched shuffle offstage, we’re all left like an awe-struck Apollo audience — still screaming for just one more encore while trying to take in the force of nature we were blessed to witness. James Brown’s face should be on money. His likeness carved on a mountain. And, most importantly, his music played forever."
+                        }
+                    }
+                }
+            ]
+        }
+    }
+}`)
+
+var search = &Search{
+	Results: SearchResults{
+		Albums: &Albums{
+			Href: "/v1/catalog/us/search?types=albums&term=james+brown&limit=1",
+			Next: "/v1/catalog/us/search?types=albums&term=james+brown&offset=1",
+			Data: []Album{
+				{
+					Id:   "900721190",
+					Type: "albums",
+					Href: "/v1/catalog/us/albums/900721190",
+					Attributes: AlbumAttributes{
+						Artwork: Artwork{
+							Width:      1400,
+							Height:     1400,
+							URL:        "https://is1-ssl.mzstatic.com/image/thumb/Music4/v4/76/85/e5/7685e5c8-9346-88db-95ff-af87bf84151b/source/{w}x{h}bb.jpg",
+							BgColor:    "ffffff",
+							TextColor1: "0a0a09",
+							TextColor2: "2a240f",
+							TextColor3: "3b3b3a",
+							TextColor4: "544f3f",
+						},
+						ArtistName: "James Brown",
+						IsSingle:   false,
+						URL:        "https://itunes.apple.com/us/album/20-all-time-greatest-hits/id900721190",
+						IsComplete: true,
+						GenreNames: []string{
+							"Soul",
+							"Music",
+							"R&B/Soul",
+							"Funk",
+						},
+						TrackCount:  20,
+						ReleaseDate: "1991-01-01",
+						Name:        "20 All-Time Greatest Hits!",
+						Copyright:   "℗ 1991 Universal Records, a Division of UMG Recordings, Inc.",
+						PlayParams: &PlayParameters{
+							Id:   "900721190",
+							Kind: "album",
+						},
+					},
+				},
+			},
+		},
+		Artists: &Artists{
+			Href: "/v1/catalog/us/search?types=artists&term=james+brown&limit=1",
+			Next: "/v1/catalog/us/search?types=artists&term=james+brown&offset=1",
+			Data: []Artist{
+				{
+					Id:   "117118",
+					Type: "artists",
+					Href: "/v1/catalog/us/artists/117118",
+					Attributes: ArtistAttributes{
+						URL:  "https://itunes.apple.com/us/artist/james-brown/id117118",
+						Name: "James Brown",
+						GenreNames: []string{
+							"R&B/Soul",
+						},
+						EditorialNotes: &EditorialNotes{
+							Standard: "The Godfather of Soul, The Hardest Working Man in Show Business, Soul Brother Number One . . . Mountain-of-a-man nicknames, no doubt, but not one of them ever did James Brown the justice he deserved. JB’s influence is so large it’s plain impossible to imagine what music today would sound like without him. James didn’t just master soul and funk; he invented them. From the on-bended-knee plea called “Try Me” straight to ground zero of the hip-hop revolution, where his screams, grunts, and funky drummer backboned every cut worth mentioning, Brown was soul power itself, an inspiration to an entire nation. Now that Mr. Dynamite has taken his last sweat-drenched shuffle offstage, we’re all left like an awe-struck Apollo audience — still screaming for just one more encore while trying to take in the force of nature we were blessed to witness. James Brown’s face should be on money. His likeness carved on a mountain. And, most importantly, his music played forever.",
+						},
+					},
+				},
+			},
+		},
+	},
+}
