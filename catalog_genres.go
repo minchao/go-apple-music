ade72abee0611cb86bf49f0efaca879c6c49231e
minchao <minchao.220@gmail.com>
2017-07-23 22:04:13 +0800

    Add genres for CatalogService

--- catalog_genres.go
+++ catalog_genres.go
@@ -1,5 +1,11 @@
 package applemusic
 
+import (
+	"context"
+	"fmt"
+	"strings"
+)
+
 type GenreAttributes struct {
 	Name string `json:"name"`
 }
@@ -12,7 +18,57 @@ type Genre struct {
 	Attributes GenreAttributes `json:"attributes"`
 }
 
+// Genres represents a list of genres.
 type Genres struct {
 	Data []Genre `json:"data"`
 	Href string  `json:"href,omitempty"`
+	Next string  `json:"next,omitempty"`
+}
+
+func (s *CatalogService) getGenres(ctx context.Context, u string) (*Genres, *Response, error) {
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	genres := &Genres{}
+	resp, err := s.client.Do(ctx, req, genres)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return genres, resp, nil
+}
+
+// GetGenre fetches a genre using its identifier.
+func (s *CatalogService) GetGenre(ctx context.Context, storefront, id string, opt *Options) (*Genres, *Response, error) {
+	u := fmt.Sprintf("v1/catalog/%s/genres/%s", storefront, id)
+	u, err := addOptions(u, opt)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return s.getGenres(ctx, u)
+}
+
+// GetGenresByIds fetches one or more genres.
+func (s *CatalogService) GetGenresByIds(ctx context.Context, storefront string, ids []string, opt *Options) (*Genres, *Response, error) {
+	u := fmt.Sprintf("v1/catalog/%s/genres?ids=%s", storefront, strings.Join(ids, ","))
+	u, err := addOptions(u, opt)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return s.getGenres(ctx, u)
+}
+
+// GetAllGenres fetches all genres for the current top charts.
+func (s *CatalogService) GetAllGenres(ctx context.Context, storefront string, opt *PageOptions) (*Genres, *Response, error) {
+	u := fmt.Sprintf("v1/catalog/%s/genres", storefront)
+	u, err := addOptions(u, opt)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return s.getGenres(ctx, u)
 }
---
+++ catalog_genres_test.go
@@ -0,0 +1,148 @@
+package applemusic
+
+import (
+	"context"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestCatalogService_GetGenre(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v1/catalog/us/genres/14", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{
+    "data": [
+        {
+            "id": "14",
+            "type": "genres",
+            "href": "/v1/catalog/us/genres/14",
+            "attributes": {
+                "name": "Pop"
+            }
+        }
+    ]
+}`))
+	})
+
+	genre := &Genres{
+		Data: []Genre{
+			{
+				Id:   "14",
+				Type: "genres",
+				Href: "/v1/catalog/us/genres/14",
+				Attributes: GenreAttributes{
+					Name: "Pop",
+				},
+			},
+		},
+	}
+
+	got, _, err := client.Catalog.GetGenre(context.Background(), "us", "14", nil)
+	if err != nil {
+		t.Errorf("Catalog.GetGenre returned error: %v", err)
+	}
+	if want := genre; !reflect.DeepEqual(got, want) {
+		t.Errorf("Catalog.GetGenre = %+v, want %+v", got, want)
+	}
+}
+
+func TestCatalogService_GetGenresByIds(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v1/catalog/us/genres", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		testFormValues(t, r, values{
+			"ids": "14,21",
+		})
+
+		w.WriteHeader(http.StatusOK)
+	})
+
+	_, _, err := client.Catalog.GetGenresByIds(context.Background(), "us", []string{"14", "21"}, nil)
+	if err != nil {
+		t.Errorf("Catalog.GetGenresByIds returned error: %v", err)
+	}
+}
+
+func TestCatalogService_GetAllGenres(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v1/catalog/us/genres", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		testFormValues(t, r, values{
+			"offset": "5",
+			"limit":  "2",
+			"l":      "en-us",
+		})
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(genresJSON)
+	})
+
+	opt := &PageOptions{
+		Offset: 5,
+		Limit:  2,
+		Options: Options{
+			Language: "en-us",
+		},
+	}
+
+	got, _, err := client.Catalog.GetAllGenres(context.Background(), "us", opt)
+	if err != nil {
+		t.Errorf("Catalog.GetAllGenres returned error: %v", err)
+	}
+	if want := genres; !reflect.DeepEqual(got, want) {
+		t.Errorf("Catalog.GetAllGenres = %+v, want %+v", got, want)
+	}
+}
+
+var genresJSON = []byte(`{
+    "data": [
+        {
+            "id": "5",
+            "type": "genres",
+            "href": "/v1/catalog/us/genres/5",
+            "attributes": {
+                "name": "Classical"
+            }
+        },
+        {
+            "id": "3",
+            "type": "genres",
+            "href": "/v1/catalog/us/genres/3",
+            "attributes": {
+                "name": "Comedy"
+            }
+        }
+    ],
+    "next": "/v1/catalog/us/genres?offset=7"
+}`)
+
+var genres = &Genres{
+	Data: []Genre{
+		{
+			Id:   "5",
+			Type: "genres",
+			Href: "/v1/catalog/us/genres/5",
+			Attributes: GenreAttributes{
+				Name: "Classical",
+			},
+		},
+		{
+			Id:   "3",
+			Type: "genres",
+			Href: "/v1/catalog/us/genres/3",
+			Attributes: GenreAttributes{
+				Name: "Comedy",
+			},
+		},
+	},
+	Next: "/v1/catalog/us/genres?offset=7",
+}
