a48da16f29517ad76e072cc0daafc0e169cf38cb
minchao <minchao.220@gmail.com>
2017-07-29 10:33:24 +0800

    Add curators for CatalogService

--- catalog_curators.go
+++ catalog_curators.go
@@ -1,10 +1,31 @@
 package applemusic
 
+import (
+	"context"
+	"fmt"
+	"strings"
+)
+
+// CuratorAttributes represents the attributes of the resource.
+type CuratorAttributes struct {
+	URL            string          `json:"url"`
+	Name           string          `json:"name"`
+	Artwork        Artwork         `json:"artwork"`
+	EditorialNotes *EditorialNotes `json:"editorialNotes,omitempty"`
+}
+
+// CuratorRelationships represents a to-one or to-many relationship from one resource object to others.
+type CuratorRelationships struct {
+	Playlists Playlists `json:"playlists"` // Default inclusion: Identifiers only
+}
+
 // Curator represents a curator of resources.
 type Curator struct {
-	Id   string `json:"id"`
-	Type string `json:"type"`
-	Href string `json:"href"`
+	Id            string               `json:"id"`
+	Type          string               `json:"type"`
+	Href          string               `json:"href"`
+	Attributes    CuratorAttributes    `json:"attributes"`
+	Relationships CuratorRelationships `json:"relationships"`
 }
 
 // Curators represents a list of curators.
@@ -12,4 +33,41 @@ type Curators struct {
 	Data []Curator `json:"data"`
 	Href string    `json:"href,omitempty"`
 	Next string    `json:"next,omitempty"`
+}
+
+func (s *CatalogService) getCurators(ctx context.Context, u string) (*Curators, *Response, error) {
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	curators := &Curators{}
+	resp, err := s.client.Do(ctx, req, curators)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return curators, resp, nil
+}
+
+// GetCurator fetches an curator using its identifier.
+func (s *CatalogService) GetCurator(ctx context.Context, storefront, id string, opt *Options) (*Curators, *Response, error) {
+	u := fmt.Sprintf("v1/catalog/%s/curators/%s", storefront, id)
+	u, err := addOptions(u, opt)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return s.getCurators(ctx, u)
+}
+
+// GetCuratorsByIds fetches one or more curators using their identifiers.
+func (s *CatalogService) GetCuratorsByIds(ctx context.Context, storefront string, ids []string, opt *Options) (*Curators, *Response, error) {
+	u := fmt.Sprintf("v1/catalog/%s/curators?ids=%s", storefront, strings.Join(ids, ","))
+	u, err := addOptions(u, opt)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return s.getCurators(ctx, u)
 }
---
+++ catalog_curators_test.go
@@ -0,0 +1,129 @@
+package applemusic
+
+import (
+	"context"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestCatalogService_GetCurator(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v1/catalog/us/curators/1107687517", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(curatorsJSON)
+	})
+
+	got, _, err := client.Catalog.GetCurator(context.Background(), "us", "1107687517", nil)
+	if err != nil {
+		t.Errorf("Catalog.GetCurator returned error: %v", err)
+	}
+	if want := curators; !reflect.DeepEqual(got, want) {
+		t.Errorf("Catalog.GetCurator = %+v, want %+v", got, want)
+	}
+}
+
+func TestCatalogService_GetCuratorsByIds(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v1/catalog/us/curators", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		testFormValues(t, r, values{
+			"ids": "976439448,1107687517",
+		})
+
+		w.WriteHeader(http.StatusOK)
+	})
+
+	_, _, err := client.Catalog.GetCuratorsByIds(context.Background(), "us", []string{"976439448", "1107687517"}, nil)
+	if err != nil {
+		t.Errorf("Catalog.GetCuratorsByIds returned error: %v", err)
+	}
+}
+
+var curatorsJSON = []byte(`{
+    "data": [
+        {
+            "id": "1107687517",
+            "type": "curators",
+            "href": "/v1/catalog/us/curators/1107687517",
+            "attributes": {
+                "url": "https://itunes.apple.com/us/curator/largeup/id1107687517",
+                "name": "LargeUp",
+                "artwork": {
+                    "width": 1080,
+                    "height": 1080,
+                    "url": "https://is2-ssl.mzstatic.com/image/thumb/Features60/v4/86/14/c5/8614c510-11a5-ef0e-4299-2edbd3987a73/source/{w}x{h}bb.jpg",
+                    "bgColor": "161113",
+                    "textColor1": "ffffff",
+                    "textColor2": "f74b2f",
+                    "textColor3": "d0cfcf",
+                    "textColor4": "ca3f29"
+                },
+                "editorialNotes": {
+                    "standard": "LargeUp is the global platform for Caribbean music and culture. Since 2009, LargeUp.com has captured the vibrant sounds, styles and flavors of the islands, spotlighting the best in reggae, dancehall, soca + beyond.",
+                    "short": "Island music and culture."
+                }
+            },
+            "relationships": {
+                "playlists": {
+                    "data": [
+                        {
+                            "id": "pl.b80bbeb133bb42aa8adf17287b0bc31b",
+                            "type": "playlists",
+                            "href": "/v1/catalog/us/playlists/pl.b80bbeb133bb42aa8adf17287b0bc31b"
+                        }
+                    ],
+                    "href": "/v1/catalog/us/curators/1107687517/playlists",
+                    "next": "/v1/catalog/us/curators/1107687517/playlists?offset=10"
+                }
+            }
+        }
+    ]
+}`)
+
+var curators = &Curators{
+	Data: []Curator{
+		{
+			Id:   "1107687517",
+			Type: "curators",
+			Href: "/v1/catalog/us/curators/1107687517",
+			Attributes: CuratorAttributes{
+				URL:  "https://itunes.apple.com/us/curator/largeup/id1107687517",
+				Name: "LargeUp",
+				Artwork: Artwork{
+					Width:      1080,
+					Height:     1080,
+					URL:        "https://is2-ssl.mzstatic.com/image/thumb/Features60/v4/86/14/c5/8614c510-11a5-ef0e-4299-2edbd3987a73/source/{w}x{h}bb.jpg",
+					BgColor:    "161113",
+					TextColor1: "ffffff",
+					TextColor2: "f74b2f",
+					TextColor3: "d0cfcf",
+					TextColor4: "ca3f29",
+				},
+				EditorialNotes: &EditorialNotes{
+					Standard: "LargeUp is the global platform for Caribbean music and culture. Since 2009, LargeUp.com has captured the vibrant sounds, styles and flavors of the islands, spotlighting the best in reggae, dancehall, soca + beyond.",
+					Short:    "Island music and culture.",
+				},
+			},
+			Relationships: CuratorRelationships{
+				Playlists: Playlists{
+					Data: []Playlist{
+						{
+							Id:   "pl.b80bbeb133bb42aa8adf17287b0bc31b",
+							Type: "playlists",
+							Href: "/v1/catalog/us/playlists/pl.b80bbeb133bb42aa8adf17287b0bc31b",
+						},
+					},
+					Href: "/v1/catalog/us/curators/1107687517/playlists",
+					Next: "/v1/catalog/us/curators/1107687517/playlists?offset=10",
+				},
+			},
+		},
+	},
+}
